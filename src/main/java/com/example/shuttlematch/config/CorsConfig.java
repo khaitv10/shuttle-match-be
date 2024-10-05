bee612c78e97ee12cd8d8fd95c1dffc1711e1f7d
khaitv10 <khaitranvu1011@gmail.com>
2024-10-05 23:06:07 +0700

    re-build

---
+++ src/main/java/com/example/shuttlematch/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.shuttlematch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
+import org.springframework.context.annotation.*;
+import org.springframework.web.cors.*;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
