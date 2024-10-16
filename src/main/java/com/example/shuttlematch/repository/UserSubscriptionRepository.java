4e25a53ba4a850110914ba5d323784f41931aa9f
khaitv10 <khaitranvu1011@gmail.com>
2024-10-17 01:29:16 +0700

    feat: deactivating expired subscriptions

--- src/main/java/com/example/shuttlematch/repository/UserSubscriptionRepository.java
+++ src/main/java/com/example/shuttlematch/repository/UserSubscriptionRepository.java
@@ -3,13 +3,19 @@ package com.example.shuttlematch.repository;
 import com.example.shuttlematch.entity.User;
 import com.example.shuttlematch.entity.UserSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserSubscriptionRepository extends JpaRepository<UserSubscription, Long> {
     Optional<UserSubscription> findByUserId(long user_id);
 
+    @Query("SELECT us FROM UserSubscription us WHERE us.endDate < :endOfToday")
+    List<UserSubscription> findExpiredSubscriptions(@Param("endOfToday") LocalDateTime endOfToday);
 
 }
